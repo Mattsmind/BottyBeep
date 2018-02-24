c50eddc3bb908cc9c27f64fbcd4b9fc9b0859c20
Mattsmind <tips.dfmedia@gmail.com>
2018-02-24 02:57:21 -0500

    Add project files.

---
+++ BottyBeep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BottyBeep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
