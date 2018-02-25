c63bdd99da0773043248a7f6ed46294c5d808ac1
Mattsmind <tips.dfmedia@gmail.com>
2018-02-24 21:39:33 -0500

    Worked out the Command Handler. Updated the Program.cs to start the bot. We are connecting. :D

--- BottyBeep/Core/CommandHandler.cs
+++ BottyBeep/Core/CommandHandler.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace BottyBeep.Core
 {
     class CommandHandler
     {
+        DiscordSocketClient _client;
+        CommandService _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            _client.MessageReceived += HandleCommandsAsync;
+        }
+
+        private async Task HandleCommandsAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+
+            if (msg == null)
+                return;
+
+            var context = new SocketCommandContext(_client, msg);
+            int argPos = 0;
+
+            if (msg.HasCharPrefix(ConfigHandler.bot.commandChar, ref argPos) || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    //Call the BotLogger system here once implemented.
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
     }
 }
--- BottyBeep/Program.cs
+++ BottyBeep/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace BottyBeep
 {
     class Program
     {
-        static void Main(string[] args)
+        DiscordSocketClient _client;
+        Core.CommandHandler _handler;
+
+        static void Main(string[] args) => 
+            new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
         {
-            Console.WriteLine(Core.ConfigHandler.bot.botToken);
+            string botToken = Core.ConfigHandler.bot.botToken;
+            if (botToken == "" || botToken == null) return;
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            _client.Log += Log;
+            
+            await _client.LoginAsync(TokenType.Bot, botToken);
+            await _client.StartAsync();
+                
+            _handler = new Core.CommandHandler();
+            await _handler.InitializeAsync(_client);
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            //Call Bot Logger here when implemented.
+            Console.WriteLine(msg.Message);
+            return Task.CompletedTask;
         }
     }
 }
