b90e8c1889800d80c41fd103495abfc02747bd94
Mattsmind <tips.dfmedia@gmail.com>
2018-02-25 01:52:59 -0500

    Echo Command added to Admin.Bot

---
+++ BottyBeep/Admin/Bot/Echo.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BottyBeep.Admin.Bot
+{
+    public class Echo : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo"), Alias("say")]
+        public async Task EchoAsync(string message)
+        {
+            await ReplyAsync(message);
+        }
+    }
+}
