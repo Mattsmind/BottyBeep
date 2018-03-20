8cf3c47165934a06ddee714d2f1e0ff1f67c5687
Mattsmind <tips.dfmedia@gmail.com>
2018-03-20 17:44:34 -0400

    Added BotNick.cs - still testing.

---
+++ BottyBeep/Admin/Bot/BotNick.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BottyBeep.Admin.Bot
+{
+    public class BotNick : ModuleBase<SocketCommandContext>
+    {
+        [RequireOwner]
+        [Command("botnick")]
+        public async Task ChangeBotNickAsync(string _nick = "")
+        {
+            await Context.Guild.CurrentUser.ModifyAsync(nick =>
+            {
+                nick.Nickname = _nick;
+            });
+        }
+    }
+}
